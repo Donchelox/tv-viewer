b4e761f5e3da7d8257fdef123421d5250761f36f
Marcelo Vasquez <marcelovazquez@MacBook-Pro-de-Marcelo-Vazquez.local>
2021-01-06 20:13:26 -0300

    new url

--- assets/app-tvusa.js
+++ assets/app-tvusa.js
@@ -1,20 +1,25 @@
 let player = document.getElementById("player");
+let nbcNewsChannel = document.getElementById("nbcnews");
+let abcNewsChannel = document.getElementById("abcnews");
+let reutersChannel = document.getElementById("reuters");
+let usaTodayChannel = document.getElementById("usatoday");
+let foxNewsChannel = document.getElementById("foxnews");
 
 
 function channelNBCNews(){
-    player.setAttribute('src','https://www.youtube.com/embed/hNHKczIYqgA?autoplay=1');
+    player.setAttribute('src','https://www.youtube.com/embed/f437y6UVjD8?autoplay=1');
 }
 function channelABCNews(){
-    player.setAttribute('src','https://www.youtube.com/embed/bwjhikkGjPU?autoplay=1');
+    player.setAttribute('src','https://www.youtube.com/embed/nWgwgkCdMnQ?autoplay=1');
 }
 function channelReuters(){
-    player.setAttribute('src','https://www.youtube.com/embed/zp1HzjT-Tbg?autoplay=1');
+    player.setAttribute('src','https://www.youtube.com/embed/QumtbKnoyHg?autoplay=1');
 }
 function channelUSAToday(){
-    player.setAttribute('src','https://www.youtube.com/embed/Oj9GpZB9cuc?autoplay=1');
+    player.setAttribute('src','https://www.youtube.com/embed/yJhiMnVpjnw?autoplay=1');
 }
 function channelCBSNews(){
-    player.setAttribute('src','https://www.youtube.com/embed/aj7ELbw8FxM?autoplay=1');
+    player.setAttribute('src','https://www.youtube.com/embed/3Fsf4aWudJk?autoplay=1');
 }
 function channelFOXNews(){
     player.setAttribute('src','https://www.youtube.com/embed/MmrGeWxd87Y?autoplay=1');
