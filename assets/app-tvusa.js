b743d09e0b0eac87bea743dc5a75f68a74430073
Marcelo Vasquez <marcelovazquez@MacBook-Pro-de-Marcelo-Vazquez.local>
2021-01-19 19:49:57 -0300

    add new url 'inauguration day' USA

--- assets/app-tvusa.js
+++ assets/app-tvusa.js
@@ -7,10 +7,10 @@ let foxNewsChannel = document.getElementById("foxnews");
 
 
 function channelNBCNews(){
-    player.setAttribute('src','https://www.youtube.com/embed/f437y6UVjD8?autoplay=1');
+    player.setAttribute('src','https://www.youtube.com/embed/gyNPdlJTWl8?autoplay=1');
 }
 function channelABCNews(){
-    player.setAttribute('src','https://www.youtube.com/embed/nWgwgkCdMnQ?autoplay=1');
+    player.setAttribute('src','https://www.youtube.com/embed/lIOizAIqZtc?autoplay=1');
 }
 function channelReuters(){
     player.setAttribute('src','https://www.youtube.com/embed/QumtbKnoyHg?autoplay=1');
@@ -19,7 +19,7 @@ function channelUSAToday(){
     player.setAttribute('src','https://www.youtube.com/embed/yJhiMnVpjnw?autoplay=1');
 }
 function channelCBSNews(){
-    player.setAttribute('src','https://www.youtube.com/embed/3Fsf4aWudJk?autoplay=1');
+    player.setAttribute('src','https://www.youtube.com/embed/SI2BwxpMPYc?autoplay=1');
 }
 function channelFOXNews(){
     player.setAttribute('src','https://www.youtube.com/embed/MmrGeWxd87Y?autoplay=1');
